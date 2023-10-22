c1a6238e707627d75ac25ebbf3b03ef537135e18
Adrian Puscuta <adrian.puscu@gmail.com>
2023-10-22 17:23:23 +0300

    Added Terminal Operations for Streams

--- src/streams/TerminalOperationsPractice.java
+++ src/streams/TerminalOperationsPractice.java
@@ -3,6 +3,7 @@ package streams;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class TerminalOperationsPractice {
 
@@ -26,6 +27,16 @@ public class TerminalOperationsPractice {
         // Optional<T> findFirst();
         Optional<Integer> any = Arrays.stream(ints).findAny();
         Optional<Integer> first = Arrays.stream(ints).findFirst();
+
+        //allMatch(), anyMatch() and noneMatch() - take in a predicate and return boolean
+        //NOTE can cause stream to hang
+        boolean allMatch = Arrays.stream(ints).allMatch(a -> a > 0);
+        boolean anyMatch = Arrays.stream(ints).anyMatch(a -> a > 0);
+        boolean noneMatch = Arrays.stream(ints).noneMatch(a -> a > 0);
+
+        //forEach() takes in a consumer, returns void
+        Stream.generate(() -> 5).limit(5).forEach(System.out::println);
+        Stream.generate(() -> 5).limit(5).forEach(a -> System.out.println(a));
     }
 
 
